2bf5b8a6ea50bfe03331e9daa83deac1ec1bf576
daOlaya <daniel@koifi.com>
2023-06-01 15:27:31 -0500

    box example

---
+++ box.py
@@ -0,0 +1,129 @@
+import pyteal as pt
+
+import beaker
+
+
+# Our custom Struct
+class Order(pt.abi.NamedTuple):
+    a1: pt.abi.Field[pt.abi.Uint64]
+    a2: pt.abi.Field[pt.abi.Uint64]
+    a3: pt.abi.Field[pt.abi.Uint64]
+    a4: pt.abi.Field[pt.abi.Uint64]
+    a5: pt.abi.Field[pt.abi.Uint64]
+    # item: pt.abi.Field[pt.abi.String]
+    # quantity: pt.abi.Field[pt.abi.Uint16]
+
+# a1 = pt.abi.Field[pt.abi.Uint64]
+# a2 = pt.abi.Field[pt.abi.Uint64]
+# a3 = pt.abi.Field[pt.abi.Uint64]
+# a4 = pt.abi.Field[pt.abi.Uint64]
+# a5 = pt.abi.Field[pt.abi.Uint64]
+
+# Order = pt.abi.StaticArray[a1, a2, a3, a4, a5]
+    # item: pt.abi.Field[pt.abi.String]
+    # quantity: pt.abi.Field[pt.abi.Uint16]
+
+
+class StructerState:
+    orders = beaker.ReservedLocalStateValue(
+        stack_type=pt.TealType.bytes,
+        max_keys=16,
+        prefix="",
+    )
+# 
+
+app = (
+    beaker.Application("Structer", state=StructerState())
+    # allow opt-in and initialise local/account state
+    .apply(beaker.unconditional_opt_in_approval, initialize_local_state=True)
+)
+
+
+@app.external
+def place_order(order_number: pt.abi.Uint8, order: Order) -> pt.Expr:
+    return app.state.orders[order_number].set(order.encode())
+
+
+# @app.external(read_only=True)
+# def read_item(order_number: pt.abi.Uint8, *, output: Order) -> pt.Expr:
+#     # return pt.Seq(
+#     #     (arr := Order()).decode(get_item(order_number)),
+#     #     output.set(arr)
+#     # )
+#     return output.set(get_item(order_number))
+    # return output.decode(get_item(order_number))
+    # return output.decode(app.state.orders[order_number])
+
+
+# @app.external(read_only=True)
+# def read_item_1(order_number: pt.abi.Uint8, *, output: pt.abi.Uint64) -> pt.Expr:
+#     return output.decode(app.state.orders[order_number][1])
+
+@app.external#(read_only=True)
+def read_item_2(order_number: pt.abi.Uint8, *, output: pt.abi.Uint64) -> pt.Expr:
+    # return output.decode(get_item(order_number))
+    return pt.Seq(
+        (new_order := Order()).decode(app.state.orders[order_number]),
+        (quant := pt.abi.Uint64()).set(new_order[1]),
+        output.set(quant.get() + pt.Int(1))
+    )
+
+@app.external
+def calc_st(order_number: pt.abi.Uint8, *, output: pt.abi.Uint64) -> pt.Expr:
+    total_sum = pt.ScratchVar(pt.TealType.uint64)
+    l = pt.ScratchVar(pt.TealType.uint64)
+    j = pt.ScratchVar(pt.TealType.uint64)
+
+    return pt.Seq(
+        total_sum.store(pt.Int(0)),
+        l.store(pt.Int(5)),
+        (order := Order()).decode(app.state.orders[order_number]),
+        pt.For(
+            j.store(pt.Int(0)),
+            j.load() < l.load(), 
+            j.store(j.load() + pt.Int(1)),
+        ).Do(
+            (x := pt.abi.Uint64()).set(j.load()),
+            (element := pt.abi.Uint64()).set(order[x]),
+            total_sum.store(total_sum.load() +  element.get()),
+        ),
+        output.set(total_sum.load())
+    )
+
+@pt.Subroutine(pt.TealType.bytes)
+def get_item(order_number: pt.abi.Uint8) -> pt.Expr:
+    # return Order().decode(app.state.orders[order_number]),
+    return pt.Seq(
+        (new_order := Order()).decode(app.state.orders[order_number]),
+        (quant := pt.abi.Uint64()).set(new_order[1]),
+        quant.encode()
+    )
+    # return app.state.orders[order_number]
+
+# @app.external(read_only=True)
+# def read_item_len(order_number: pt.abi.Uint8, *, output: Order) -> pt.Expr:
+#     return pt.Seq(
+#         arr := app.state.orders[order_number],
+
+#     )
+
+
+# @app.external
+# def increase_quantity(order_number: pt.abi.Uint8, *, output: Order) -> pt.Expr:
+#     return pt.Seq(
+#         # Read the order from state
+#         (new_order := Order()).decode(app.state.orders[order_number]),
+#         # Select out in the quantity attribute, its a TupleElement type
+#         # so needs to be stored somewhere
+#         (quant := pt.abi.Uint16()).set(new_order.quantity),
+#         # Add 1 to quantity
+#         quant.set(quant.get() + pt.Int(1)),
+#         (item := pt.abi.String()).set(new_order.item),
+#         # We've gotta set all of the fields at the same time, but we can
+#         # borrow the item we already know about
+#         new_order.set(item, quant),
+#         # Write the new order to state
+#         app.state.orders[order_number].set(new_order.encode()),
+#         # Write new order to caller
+#         output.decode(new_order.encode()),
+#     )
---
+++ demo.py
@@ -0,0 +1,71 @@
+# import struct
+from algosdk.abi import ABIType
+
+from beaker import client, sandbox
+
+import box
+
+
+def main() -> None:
+    # Create a codec from the python sdk
+    order_codec = ABIType.from_string(str(box.Order().type_spec()))
+
+    acct = sandbox.kmd.get_accounts().pop()
+
+    # Create an Application client containing both an algod client and my app
+    app_client = client.ApplicationClient(
+        sandbox.get_algod_client(), box.app, signer=acct.signer
+    )
+
+    # Create the applicatiion on chain, set the app id for the app client
+    app_id, app_addr, txid = app_client.create()
+    print(f"Created App with id: {app_id} and address addr: {app_addr} in tx: {txid}")
+
+    # Since we're using local state, opt in
+    app_client.opt_in()
+
+    # Passing in a dict as an argument that should take a tuple
+    # according to the type spec
+    order_number = 12
+    # order = {"quantity": 8, "item": "cubes"}
+    order = [1000000, 2000000, 3000000, 4000000, 5000000]
+    app_client.call(box.place_order, order_number = order_number, order = order)
+
+    # Get the order from the state field
+    state_key = order_number.to_bytes(1, "big")
+    stored_order = app_client.get_local_state(raw=True)[state_key]
+    # assert isinstance(stored_order, bytes)
+    state_decoded = order_codec.decode(stored_order)
+
+    print(
+        "We can get the order we stored from local "
+        f"state of the sender: {state_decoded}"
+    )
+
+    # Or we could call the read-only method, passing the order number
+    result = app_client.call(box.read_item_2, order_number=order_number)
+    # abi_decoded = order_codec.decode(result.raw_value)
+    # print(f"Decoded result: {abi_decoded}")
+    print(result.return_value)
+
+    result = app_client.call(box.calc_st, order_number=order_number)
+    print(order_codec.decode(result.return_value))
+
+    # Update the order to increase the quantity
+    # result = app_client.call(box.increase_quantity, order_number=order_number)
+    # increased_decoded = order_codec.decode(result.raw_value)
+    # print(
+    #     "Let's add 1 to the struct, update state, and "
+    #     f"return the updated version: {increased_decoded}"
+    # )
+
+    # And read it back out from state
+    # state_key = order_number.to_bytes(1, "big")
+    # stored_order = app_client.get_local_state(raw=True)[state_key]
+    # assert isinstance(stored_order, bytes)
+    # state_decoded = order_codec.decode(stored_order)
+    # print(f"And it's been updated: {state_decoded}")
+
+
+if __name__ == "__main__":
+    main()
